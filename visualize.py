e04d2f3669af91642b51486635843c221d8aaabd
bakiuzun <uzunbaki00@gmail.com>
2026-04-04 17:39:05 +0200

    waypoints following worked!

--- visualize.py
+++ visualize.py
@@ -1,15 +1,16 @@
+from math import dist
 import mujoco
 import mujoco.viewer
 import jax
 import jax.numpy as jnp
 import time
 from flax import nnx
-import tkinter as tk 
 from models import ActorCritic
 from utils import * 
 from structs import ModelSpec
 from envs import rewards
 from envs import utils as env_utils
+import numpy as np 
 
 # Load mode
 actorSpec = ModelSpec(
@@ -27,9 +28,7 @@ criticSpec = ModelSpec(
 model = ActorCritic(17, 4,actorSpec,criticSpec,nnx.Rngs(0))
 graphdef, params, non_params = nnx.split(model, nnx.Param, ...)
 
-path = "rdmenv_quatrad01_lr0.0003_gm0.99_steps100000000.0.pt"
-
-path = "mt.pt"
+path = "baseline.pt"
 model = load_model(f'checkpoints/{path}', graphdef)
 print(f"Model: {model.log_std} ")
 # Setup MuJoCo (pas MJX, le vrai renderer)
@@ -56,11 +55,34 @@ tau = 0.0
 # if alpha = 1 -> we take directly the output 
 alpha = timestep / (tau + timestep) # how fast we update 
 
+waypoints = jnp.array([-1,3.4,2.])
+
 prev_obs = jnp.zeros(19)
 prev_act = None 
 step_counter = 0 
 
 current_ctrl = jnp.zeros(4)
+
+import time 
+
+
+
+def new_waypoints(x,y,z):
+  return jnp.array([x + 2,y+0.5,z])
+
+
+def randomize_waypoints(key,x: tuple = (-5,5),y:tuple = (-5,5),z:tuple = (0.1,5)):
+    x_min,x_max = x
+    y_min,y_max = y 
+    z_min,z_max = z 
+
+    keys = jax.random.split(key,3)
+
+    x_rand = jax.random.uniform(keys[0],shape=(),minval=x_min,maxval=x_max)
+    y_rand = jax.random.uniform(keys[1],shape=(),minval=y_min,maxval=y_max)
+    z_rand = jax.random.uniform(keys[2],shape=(),minval=z_min,maxval=z_max)
+    
+    return jnp.array([x_rand,y_rand,z_rand])
 
 with mujoco.viewer.launch_passive(mj_model, mj_data) as viewer:
     mujoco.mj_resetData(mj_model, mj_data)
@@ -68,21 +90,47 @@ with mujoco.viewer.launch_passive(mj_model, mj_data) as viewer:
 
     while viewer.is_running():
         # Get obs
+        viewer.user_scn.ngeom = 0 
+        geom_idx = 0 
+
+
+        mujoco.mjv_initGeom(
+          viewer.user_scn.geoms[geom_idx],
+          type=mujoco.mjtGeom.mjGEOM_SPHERE,
+          size=[0.15, 0, 0],
+          pos=np.array(waypoints, dtype=np.float64),
+          mat=np.eye(3).flatten(),
+          rgba=np.array([1, 0, 0, 0.8], dtype=np.float32),
+        )
+        
+        geom_idx += 1
+        
+        viewer.user_scn.ngeom = geom_idx
         sd = mj_data.sensordata
         rot_mat = env_utils.quat_to_rotmat(sd[quat_slice])
+      
+        distt = waypoints - mj_data.qpos[:3]
+      
+        if jnp.linalg.norm(distt) < 1:
+          key = jax.random.fold_in(rng, int(time.time()))
         
+          waypoints = randomize_waypoints(key,z=(1,5))
+          
+        body_frame = rot_mat.reshape((3,3)).T @ distt 
         obs = jnp.concatenate([
             rot_mat,
             jnp.array(sd[accel_slice]),
             jnp.array(sd[gyro_slice]),
             mj_data.qpos[2:3],
-            mj_data.qvel[0:3]
+            mj_data.qvel[0:3],
+            body_frame
         ])
 
 
         rng, k = jax.random.split(rng)
         action, _, _ = model(obs, k)
 
+        
         if prev_act is None:
             prev_act = action 
         
@@ -94,6 +142,7 @@ with mujoco.viewer.launch_passive(mj_model, mj_data) as viewer:
             mj_data.ctrl[:] = current_ctrl
 
             mujoco.mj_step(mj_model, mj_data)
+       
         
         viewer.sync() # je update le viewer pour quil se met exactement en 0.012 
 
