5436cc2c98bff3fe6722522af9bbb7fe96184921
bakiuzun <uzunbaki00@gmail.com>
2026-04-26 19:43:51 +0200

    added dino vision model first try

--- models/vision.py
+++ models/vision.py
@@ -11,37 +11,44 @@ class Vision():
     def __init__(self):
 
 
-    self.model = torch.hub.load(
-        'facebookresearch/dinov2', 
-        'dinov2_vits14', 
-        pretrained=False
-    )
-    self.model.load_state_dict(torch.load(LOCAL_WEIGHT_PATH))
-    self.model.eval()
+        self.model = torch.hub.load(
+            'facebookresearch/dinov2', 
+            'dinov2_vits14', 
+            pretrained=False
+        )
+        self.model.load_state_dict(torch.load(LOCAL_WEIGHT_PATH))
+        self.model.eval()
 
-    self.transform = T.Compose([
-        T.Resize((224,224)),
-        T.ToTensor(),
-        T.Normalize(mean=(0.485,0.456,0.406),
-                    std=(0.229,0.224,0.225)),
-    ])
+        self.transform = T.Compose([
+            T.Resize((224,224)),
+            T.ToTensor(),
+            T.Normalize(mean=(0.485,0.456,0.406),
+                        std=(0.229,0.224,0.225)),
+        ])
 
 
     def get_features(self,img_folder):
-
         paths = os.listdir(img_folder)
 
         for p in paths:    
             img =  Image.open(f"{img_folder}/{p}").convert('RGB')
-            img = transform(yo).unsqueeze(0)
+            img = self.transform(img).unsqueeze(0)
 
             with torch.no_grad():
+                print("img shape = ",img.shape)
+                
+                # a tuple 
+                last_4_blocks = self.model.get_intermediate_layers(img,n=4)
+                
+                # blocks (1,256,384)
+                block1 = last_4_blocks[0]
+                block2 = last_4_blocks[1]
+                block3 = last_4_blocks[2]
+                block4 = last_4_blocks[3]
 
-                last_4_blocks = model.get_intermediate_layers(img,n=4)
-                print("Last 4 ",type(last_4_blocks))
 
 vision = Vision()
 
-vision.get_features("target_imgs")
+vision.get_features("../target_imgs")
 
 
